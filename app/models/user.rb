29346969c6ef116e7e4adfe2f490146bea1c4488
Matt Cloyd <cloydster@gmail.com>
2016-01-12 10:27:43 -0500

    Add a User#member_of? helper to determine org membership.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,17 @@ class User < ActiveRecord::Base
     @null ||= new(email: "<Null User>")
   end
 
+  def active_memberships
+    memberships.select(&:active?)
+  end
+
+  def member_of?(*organizations)
+    organizations = Array(organizations).flatten
+    joined_orgs = active_memberships.map(&:organization)
+    member_of = organizations.select{|org| joined_orgs.include?(org)}
+    member_of.empty? ? false : member_of
+  end
+
   def avatar
     user = %w(mark lena lindsay molly eve).sample
     "http://semantic-ui.com/images/avatar2/small/#{user}.png"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,4 +19,22 @@ class UserTest < ActiveSupport::TestCase
     user.save
     assert_not_empty user.hashed_email
   end
+
+  test "#member_of? for single organization" do
+    org = organizations(:mapc)
+    refute user.member_of?(org)
+    user.memberships.build(organization: org)
+    refute user.member_of?(org)
+    user.memberships.first.activated
+    assert user.member_of?(org)
+  end
+
+  test "#member_of? for multiple organization" do
+    org = organizations(:mapc)
+    org2 = organizations(:massit)
+    user.memberships.build(organization: org,  state: :active)
+    user.memberships.build(organization: org2, state: :active)
+    assert user.member_of?(org, org2)
+    assert_equal([org, org2], user.member_of?([org, org2]))
+  end
 end
